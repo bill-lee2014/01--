1c91f3cd9a1438dde10f89da8c2c43b01ae1b200
apple <apple@Teacher-316.local>
2014-08-11 11:35:39 +0800

    Initial Commit

---
+++ 01-加法计算器/HMViewController.h
@@ -0,0 +1,13 @@
+//
+//  HMViewController.h
+//  01-加法计算器
+//
+//  Created by apple on 14-8-11.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMViewController : UIViewController
+
+@end
---
+++ 01-加法计算器/HMViewController.m
@@ -0,0 +1,29 @@
+//
+//  HMViewController.m
+//  01-加法计算器
+//
+//  Created by apple on 14-8-11.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import "HMViewController.h"
+
+@interface HMViewController ()
+
+@end
+
+@implementation HMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
