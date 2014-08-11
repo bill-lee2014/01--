15043b0c8bdc4554d1c9008af34f404f67d48d5c
apple <apple@Teacher-316.local>
2014-08-11 15:25:06 +0800

    IBAction & IBOutlet介绍 程序启动Storyboard的选择

--- 01-加法计算器/HMViewController.h
+++ 01-加法计算器/HMViewController.h
@@ -10,6 +10,14 @@
 
 /**
  用来声明属性和方法
+ 
+ IBAction & IBOutlet
+ 
+ IB: Interface Builder
+ 
+ 1> IBAction : 如果需要监听按钮的点击方法，使用IBAction，就可以与IB进行连线，连线之后，在点击按钮时，就会执行相应的方法
+ 2> IBOutlet : 如果需要获得/修改界面上控件的属性，使用IBOutlet，就可以与IB进行连线
+ 
  */
 @interface HMViewController : UIViewController
 
